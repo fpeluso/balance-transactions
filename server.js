1e12f69f451a6f384c5d4880a213b70da02754a6
Fabio Peluso <peluso.f@gmail.com>
2024-09-20 14:14:44 +0200

    change listen port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ fastify.register(transactionsRoutes)
 fastify.register(categoriesRoutes)
 
 // Run the server!
-fastify.listen({ port: 3000 }, function (err, address) {
+fastify.listen({ port: 3001 }, function (err, address) {
     if (err) {
         fastify.log.error(err)
         process.exit(1)
